9bba4bb99833fc9bd7e9a2a9fc21e73780fb6dd2
Kristine Kofoed <kris894d@stud.kea.dk>
2021-06-14 17:28:03 +0200

    Færdig med burgermenu

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+window.addEventListener("load", sidenVises);
+
 let slideIndex = 0;
 showSlides();
 
@@ -13,4 +15,22 @@ function showSlides() {
   }
   slides[slideIndex - 1].style.display = "block";
   setTimeout(showSlides, 4000); // Change image every 4 seconds
+}
+
+function sidenVises() {
+  console.log("sidenVises");
+  document.querySelector("#menuknap").addEventListener("click", toggleMenu);
+}
+
+function toggleMenu() {
+  console.log("toggleMenu");
+  document.querySelector("#menu").classList.toggle("hidden");
+
+  let erSkjult = document.querySelector("#menu").classList.contains("hidden");
+
+  if (erSkjult == true) {
+    document.querySelector("#menuknap").textContent = "☰";
+  } else {
+    document.querySelector("#menuknap").textContent = "X";
+  }
 }
